fd0f56925873b6d328e090ae4348a8ba2b3e54e0
Jeremy Gray <jeremygray@utexas.edu>
2019-11-14 07:18:28 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -10,7 +10,7 @@ def join_ingredients(src)
   while row_count<src.count do
     element_count=0
     while element_count<src.count do
-      final=["I love "+src[element_count][0]+" and "+ src[element_count][1]+" on my pizza"]
+      final(element_count)=["I love "+src[element_count][0]+" and "+ src[element_count][1]+" on my pizza"]
       element_count += 1
     end
     row_count += 1
